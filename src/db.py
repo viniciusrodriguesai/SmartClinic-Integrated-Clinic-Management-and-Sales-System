5fda8b5e06ac8281bdc7eb0d11219c24e71fd51f
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-03-15 15:13:26 -0300

    Adding the database port

--- src/db.py
+++ src/db.py
@@ -1,10 +1,13 @@
 import os
 from contextlib import contextmanager
-
+from pathlib import Path
 import mysql.connector
 from dotenv import load_dotenv
 
-load_dotenv("secreto.env")
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / "secreto.env"
+
+load_dotenv(ENV_PATH)
 
 
 def _get_env(nome_variavel: str) -> str:
@@ -25,6 +28,7 @@ def get_conn():
     try:
         conn = mysql.connector.connect(
             host=_get_env("DB_HOST"),
+            port=int(_get_env("DB_PORT")),
             user=_get_env("DB_USER"),
             password=_get_env("DB_PASSWORD"),
             database=_get_env("DB_NAME"),
