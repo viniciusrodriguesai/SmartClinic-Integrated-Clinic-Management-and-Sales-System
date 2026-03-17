6b139d583587d31054966d0b3560ea3aece44f1f
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-03-17 13:28:28 -0300

    Adicionando a interface

--- interface/interface.py
+++ interface/interface.py
@@ -15,7 +15,7 @@ class Application():
         self.root.geometry("700x500")
         self.root.resizable(True, True)
         self.root.maxsize(width=900, height=700)
-        self.root.minsize(width=400, height=300)
+        self.root.minsize(width=500, height=400)
     def frames_da_tela(self):
         self.frame_1 = Frame(self.root, bd = 4, bg="#FFFFFF", highlightbackground="#F9F9F9", highlightthickness=3)
         
@@ -42,9 +42,32 @@ class Application():
         self.bt_excluir.place(relx=0.8, rely=0.01, relwidth=0.1, relheight=0.15)
         #Criação do label e entrada do codigo
         self.lb_codigo = Label(self.frame_1, text="Código",)
-        self.lb_codigo.place(relx=0.01, rely=0.2, relwidth=0.1, relheight=0.15)
+        self.lb_codigo.place(relx=0.05, rely=0.35)
 
         self.codigo_entry = Entry(self.frame_1)
-        self.codigo_entry.place(relx=0.1, rely=0.25, relwidth=0.1, relheight=0.15)
+        self.codigo_entry.place(relx=0.05, rely=0.45, relwidth=0.8,)
+        
+        #Criação do label e entrada do nome
+        self.lb_nome = Label(self.frame_1, text="Nome",)
+        self.lb_nome.place(relx=0.05, rely=0.6)
+
+        self.nome_entry = Entry(self.frame_1)
+        self.nome_entry.place(relx=0.05, rely=0.7, relwidth=0.8,)
+
+        #Criação do label e entrada do Telefone
+        self.lb_telefone = Label(self.frame_1, text="Telefone",)
+        self.lb_telefone.place(relx=0.05, rely=0.6)
+
+        self.telefone_entry = Entry(self.frame_1)
+        self.telefone_entry.place(relx=0.05, rely=0.7, relwidth=0.4,)
+
+        #Criação do label e entrada do Email
+        self.lb_email = Label(self.frame_1, text="Email",)
+        self.lb_email.place(relx=0.5, rely=0.6)
+
+        self.email_entry = Entry(self.frame_1)
+        self.email_entry.place(relx=0.5, rely=0.7, relwidth=0.4,)
+
+
 
 Application()
